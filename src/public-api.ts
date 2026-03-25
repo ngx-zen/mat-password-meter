6222806e2a9ec51de7ba930e881e66b91ec05c69
cpeteradison <cpeteradison@gmail.com>
2026-03-26 01:27:34 +0800

    refactor: removed internal methods in public api

--- src/public-api.ts
+++ src/public-api.ts
@@ -12,14 +12,7 @@ export type {
   DisabledOptionKey,
 } from './shared/types';
 export { DEFAULT_PASSWORD_RULE_OPTIONS, DEFAULT_PASSWORD_METER_MESSAGES } from './shared/types';
-export {
-  evaluateRules,
-  scoreFromChecks,
-  buildDisabledOptionsNudge,
-  resolveDisabledOptionsNudge,
-  getMissingDisabledKeys,
-  DISABLED_KEY_LABELS,
-} from './shared/rules.utils';
+export { evaluateRules, scoreFromChecks, resolveDisabledOptionsNudge } from './shared/rules.utils';
 export { scoreToColor, scoreToLabel, ZXCVBN_SCORE_MAP } from './shared/meter.utils';
 export type { StrengthColor } from './shared/meter.utils';
 export { METER_STYLES } from './shared/meter.styles';
